cb1c874ed7ccc4925f635d980554e170915b4425
Stanislaw Gupaliuk <gupalyuk@informatics.ru>
2024-08-18 21:55:12 +0300

    Добавлена авторизация, изменены шаблоны и работа с профилем.

--- final_prom_project/final_prom_project/urls.py
+++ final_prom_project/final_prom_project/urls.py
@@ -1,14 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from newsfeed import views
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('about/', views.about_view, name='about'),
-    path('news/', include('newsfeed.urls')),
-    path('registration/', views.registration_view, name='reg'),
+	path('admin/', admin.site.urls),
+	path('', views.home_view, name='home'),
+	path('about/', views.about_view, name='about'),
+	path('news/', include('newsfeed.urls')),
+	path('registration/', views.registration_view, name='reg'),
+	path('login/', views.login_view, name='login'),
+	path('profile/', views.profile_view, name='profile'),
+	path('logout/', views.logout_view, name='logout')
 
 ]
